e8beefb0090f1165cf0bf4d0c086a31208b23d20
MobGeko <paulxmob@gmail.com>
2023-04-29 12:50:24 +0200

    Third commit, changed the content of App.js by adding semantic html and added meta tags and og:tags in index.html

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,11 +1,41 @@
-
+import './Header.js';
+import './Nav.js';
+import './Main.js';
+import './Footer.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Header>
+        <img src="./logo.jpeg" alt="Little-Lemon-Logo" />
+        <Nav>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/Menu">Menu</a></li>
+            <li><a href="/Reservation">Reservation</a></li>
+            <li><a href="/Order-Online">Order Online</a></li>
+            <li><a href="/Login">Login</a></li>
+          </ul>
+        </Nav>
+      </Header>
+      <Main>
+      </Main>
+      <Footer>
+        <Nav>
+          <h3>Doormat Navigation</h3>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/Menu">Menu</a></li>
+            <li><a href="/Reservation">Reservation</a></li>
+            <li><a href="/Order-Online">Order Online</a></li>
+            <li><a href="/Login">Login</a></li>
+          </ul>
+        </Nav>
+      </Footer>ù
+    </>
   );
 }
 
